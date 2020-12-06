295254557ff9fca3056f6bf941f2d2b003bb689b
Heimer Martinez <hhmartinez@uao.edu.co>
2020-12-06 17:37:31 -0500

    added post section

--- wp-content/themes/takehome/front-page.php
+++ wp-content/themes/takehome/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-<main class="box">
+<main>
 
-	<section id="get-ahead-section">
+	<section id="get-ahead-section" >
 
 		<aside id="get-ahead">
 		
@@ -15,11 +15,64 @@
 		</div>
 	</section>
 
-	<section id="based-data">
-		<div class="card"><span class="icon icon-ticket"></span><div><h3></h3><small></small></div></div>
-		<div class="card"><span class="icon icon-users"></span><div><h3></h3><small></small></div></div>
-		<div class="card"><span class="icon icon-accessibility"></span><div><h3></h3><small></small></div></div>
-		<div class="card"><span class="icon icon-wrench"></span><div><h3></h3><small></small></div></div>
+	<section id="based-data" class="box center-box" >
+		<h3 class="box">Based on data from:</h3>
+		<div class="card"><span class="icon icon-ticket"></span><div><h3>200</h3><small>tickets</small></div></div>
+		<div class="card"><span class="icon icon-users"></span><div><h3>320M</h3><small>customers</small></div></div>
+		<div class="card"><span class="icon icon-accessibility"></span><div><h3>320M</h3><small>agents</small></div></div>
+		<div class="card"><span class="icon icon-wrench"></span><div><h3>320</h3><small>customers</small></div></div>
+	</section>
+
+
+	<section id="see-where" class="center-box">
+		<h2 class="center-text">See where you stand</h2>
+		<p class="center-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper, leo dapibus dictum tempor, urna arcu blandit risus,</p>
+	<label for="industry">Industry</label>
+	<div class="input-container">
+			<i class="icon icon-home"> </i> 
+			<input class="input-field" list="industries" name="industry" id="industry" placeholder="Select">
+	</div>
+		<datalist id="industries">
+			<option value="Gas">
+			<option value="Automobile">
+			<option value="Food">
+			<option value="Health">
+		</datalist>
+		
+	</section>
+	<section id="research-post" class="center-box">
+
+	<h2 class="center-text">Learn from our research</h2>
+		<p class="center-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper, leo dapibus dictum tempor, urna arcu blandit risus,</p>
+
+		<article>
+			<figure><img src="wp-content/themes/takehome/assets/img/Group-1.svg" alt=""></figure>
+			<div>
+				<h2>Research Investigation regarding COVID-19 vaccine and the way it impacts LATAM.</h2>
+				<p><a href="">Read report</a></p>
+			</div>
+		</article>
+		<article>
+			<figure><img src="wp-content/themes/takehome/assets/img/Group-1.svg" alt=""></figure>
+			<div>
+				<h2>Research Investigation regarding COVID-19 vaccine and the way it impacts LATAM.</h2>
+				<p><a href="">Read report</a></p>
+			</div>
+		</article>
+		<article>
+			<figure><img src="wp-content/themes/takehome/assets/img/Group-1.svg" alt=""></figure>
+			<div>
+				<h2>Research Investigation regarding COVID-19 vaccine and the way it impacts LATAM.</h2>
+				<p><a href="">Read report</a></p>
+			</div>
+		</article>
+		<article>
+			<figure><img src="wp-content/themes/takehome/assets/img/Group-1.svg" alt=""></figure>
+			<div>
+				<h2>Research Investigation regarding COVID-19 vaccine and the way it impacts LATAM.</h2>
+				<p><a href="">Read report</a></p>
+			</div>
+		</article>
 	</section>
 	
 
